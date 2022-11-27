a4c6b3b3ad221ce7fde4c7ad1bbcc34701cff9d5
briidee3 <briidee3@gmail.com>
2022-11-27 15:10:15 -0500

    properly initialized the jsonp file in main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@
 
 window.onload = () => {
 
-    // instantiate each individual slide using data.json
+    // load data.json (JSONP file) into the webpage
+    let body = document.querySelector(`body`);
+    let jsonpScript = document.createElement(`script`);
+    jsonpScript.setAttribute(`src`, `json/data.json`);
+    jsonpScript.setAttribute(`id`, `jsonpSrc`);
+    body.appendChild(jsonpScript);
 
+};
 
+// utilize the jsonp (data.json) file and organize the slides
+// for the carousel for each element in the jsonp file
+let albums = (json) => {
+
+    console.log(json);
 };
